97d2c29dc9f714b15f2f60322b0f3178952ba1c6
nathan <nyeo700@gmail.com>
2026-06-18 01:14:22 +0800

    fix inf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,7 @@
-import { useDispatch } from "react-redux";
 import HowToUse from "../components/HowToUse";
 import PixelFlower from "../images/pixelFlower.png";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { useEffect, useMemo } from "react";
-
-const KNOWN_ROUTES = new Set(["contests", "user", "problem"]);
 
 export default function Home() {
-  let [searchParams] = useSearchParams();
-  let path = searchParams.get("");
-
-  const urlParams = useMemo(() => {
-    if (!path) return [];
-    const parts = path.split("/").filter(Boolean);
-    return parts;
-  }, [path]);
-
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (urlParams.length === 0) return;
-
-    let target = null;
-
-    if (urlParams.length === 1) {
-      if (KNOWN_ROUTES.has(urlParams[0])) {
-        target = `/${urlParams[0]}`;
-      } else {
-        target = `/user/${urlParams[0]}`;
-      }
-    } else if (urlParams.length >= 2) {
-      if (urlParams[0] === "user") {
-        target = `/user/${urlParams[1]}`;
-      } else if (urlParams[0] === "problem") {
-        target = `/problem/${urlParams[1]}/${urlParams[2] ?? ""}`;
-      } else {
-        target = `/problem/${urlParams[0]}/${urlParams[1]}`;
-      }
-    }
-
-    if (target) {
-      navigate(target, { replace: true }); // replace: true prevents history loop
-    }
-  }, [navigate, urlParams]);
-
   return (
     <div className="">
       <div
@@ -58,7 +16,6 @@ export default function Home() {
           <img className="h-14 w-auto" src={PixelFlower} alt="" />
           Explorer
         </div>
-
         <div className="text-xs">
           <HowToUse />
         </div>
